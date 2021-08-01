b4255f82bcb8184ef7bf34f621389d6113e66f48
Amjed Ali <amjedmgm@gmail.com>
2021-08-01 15:00:45 +0530

    Updated Endpoints and Docs

--- POLMO Sofware/Fast API - Backend/api/sensornode.py
+++ POLMO Sofware/Fast API - Backend/api/sensornode.py
@@ -1,15 +1,17 @@
-from fastapi import Depends, HTTPException, APIRouter, BackgroundTasks, Header
+from typing import List
+
+from fastapi import Depends, HTTPException, APIRouter, Header
 from starlette import status
 
 from models.sensordata import SensorReading
-from services.sensor import get_last_value
+from services.sensor import get_last_value, get_last_values
 
 r = APIRouter()
-
+tagname = 'SensorNode'
 valid_sensors = ['NO2', 'CO2', 'CH4', 'CO', 'PM1.0', 'PM2.0', 'PM10', 'O3', 'SO2']
 
 
-@r.get('/sensor/{node}/{pollutant}/last/', response_model=SensorReading, tags=['SensorNode'])
+@r.get('/sensor/{node}/{pollutant}/last/', response_model=SensorReading, tags=[tagname])
 async def get_last_reading(node: str, pollutant: str):
     if pollutant not in valid_sensors:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Sensor name is invalid")
@@ -18,3 +20,12 @@ async def get_last_reading(node: str, pollutant: str):
         raise HTTPException(status_code=status.HTTP_206_PARTIAL_CONTENT, detail="No data available")
     return res
 
+
+@r.get('/sensor/{node}/{pollutant}/days/{days}', response_model=List[SensorReading], tags=[tagname])
+async def get_last_days_reading(node: str, pollutant: str, days:int):
+    if pollutant not in valid_sensors:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Sensor name is invalid")
+    res = await get_last_values(pollutant, node, days)
+    if not res:
+        raise HTTPException(status_code=status.HTTP_206_PARTIAL_CONTENT, detail="No data available")
+    return res
--- POLMO Sofware/Fast API - Backend/main.py
+++ POLMO Sofware/Fast API - Backend/main.py
@@ -6,9 +6,30 @@ from views import home
 from config import settings
 import uvicorn
 
-app = fastapi.FastAPI(title="My Super Project",
-                      description="This is a very fancy project, with auto docs for the API and everything",
-                      version="2.5.0", )
+tags_metadata = [
+    {
+        "name": "User",
+        "description": "User registration and Authentication REST API Routes.",
+    },
+    {
+        "name": "SensorNode",
+        "description": "Endpoint related to sensor nodes. Here you can fetch all data related to sensor nodes. Data "
+                       "are directly fetched from databases. This API endpoint doesn't have any direct link to "
+                       "sensornodes and its hardware.",
+        "externalDocs": {
+            "description": "Full Project GitHub Link",
+            "url": "https://github.com/HarinarayananP/Air-polution-monitoring",
+        },
+    },
+]
+app = fastapi.FastAPI(title="POLMO",
+                      openapi_tags=tags_metadata,
+                      description="Air pollution monitoring system",
+                      version="0.1.0",
+                      license_info={
+                          "name": "Apache 2.0",
+                          "url": "https://www.apache.org/licenses/LICENSE-2.0.html"},
+                     )
 
 
 
